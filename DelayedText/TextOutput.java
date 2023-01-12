bfe3ac2f90834fc516f2041b1f859e64bc3dcc3c
Sot-Mits <86083227+Sot-Mits@users.noreply.github.com>
2023-01-12 21:42:55 +0100

    Update TextOutput.java
    Added comments

--- DelayedText/TextOutput.java
+++ DelayedText/TextOutput.java
@@ -5,8 +5,8 @@ import java.io.File;
 
 public class TextOutput {
     static String filePath = System.getProperty("user.dir") + File.separator;
-    static ArrayList<String> output = FileConverter(filePath + "input.txt");
-    static ArrayList<Integer> delays = DelayConverter(filePath + "delay.txt");
+    static ArrayList<String> output = FileConverter(/*filePath + */"input.txt");
+    static ArrayList<Integer> delays = DelayConverter(/*filePath + */"delay.txt");
 
     public static void main(String[] args) {
         clear();
@@ -14,10 +14,12 @@ public class TextOutput {
         delay(delays.get(5));
     } 
 
+    //Copies the contents of input.txt in an ArrayList
     static ArrayList<String> FileConverter(String fileName) {
         ArrayList<String> fileContents = new ArrayList<String>();
 
         try {
+            //Can't remember the exact reason why I chose BufferedReader out of all the other choices.
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
             String line;
@@ -28,7 +30,8 @@ public class TextOutput {
             
         } 
 
-        catch (Exception exception) {
+        //General exception because I don't need the file not found exception or whatever it's called.
+        catch (Exception fileNotFoundException) {
             System.out.println("Enter a file.");
             System.exit(0);
         }
@@ -36,6 +39,8 @@ public class TextOutput {
     } 
 
     static ArrayList<Integer> DelayConverter(String delayName) {
+        
+        //This is here so the program still has values to use for delays if the user doesn't provide them in the delay.txt file
         ArrayList<Integer> delayContents = new ArrayList<Integer>();
         //0
         delayContents.add(2000);
@@ -50,6 +55,7 @@ public class TextOutput {
         //5
         delayContents.add(10000);
         
+        //Gets the user defined values if they exist
         try {
             BufferedReader reader = new BufferedReader(new FileReader(delayName));
 
@@ -64,6 +70,7 @@ public class TextOutput {
             reader.close();
         } 
 
+        //Catch is empty because it doesn't need to do anything
         catch (Exception exception) {}
         return delayContents;
     }
@@ -75,6 +82,7 @@ public class TextOutput {
         delay(delays.get(0));
     }
 
+    //Thread.sleep() says I should wrap it in a try catch so that's why this function exists - does everything together in the way it should!
     static void delay(int lineDelayInMillis) {
         try {
             Thread.sleep(lineDelayInMillis);
@@ -84,12 +92,16 @@ public class TextOutput {
         }
     }
 
+    /* Simple array printer
+       Didn't really need to create a function for it but hey whatever */
     static void fileOutput(ArrayList<String> fileOutputArray) {
         for (int i = 0; i < fileOutputArray.size(); i++) {
             output(fileOutputArray.get(i));
         }
     }
 
+    /* Arguably the most important function for this program and the 
+       Prints each line in the array (i.e. each line in the text) individually; thought it'd be more reusable that way. */
     static void output(String message) {
         for (int i = 0; i < message.length(); i++) {
             System.out.print(message.charAt(i)); 
